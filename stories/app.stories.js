63095e935f3b9ab3b8bee705d3202839c31eff02
Bryan Ollendyke <bto108@psu.edu>
2022-01-19 15:52:31 -0500

    Initial commit

---
+++ stories/app.stories.js
@@ -0,0 +1,21 @@
+import { html } from 'lit';
+import '../src/app.js';
+
+export default {
+  title: 'Project',
+  component: 'learning-card',
+  argTypes: {
+    type: { control: 'text' },
+  },
+};
+
+function Template({ type = 'math', slot }) {
+  return html` <learning-card type="${type}"> ${slot} </learning-card> `;
+}
+export const Card = Template.bind({});
+
+export const ScienceCard = Template.bind({});
+ScienceCard.args = {
+  type: 'science',
+  slot: html`<p>slotted content that should render</p>`,
+};
