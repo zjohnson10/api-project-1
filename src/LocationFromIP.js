0afb8c1120e99352beb044dd09a4342ed4d016a0
btopro <btopro@outlook.com>
2022-01-20 01:16:39 -0500

    starting point of several elements

---
+++ src/LocationFromIP.js
@@ -0,0 +1,65 @@
+// dependencies / things imported
+import { LitElement, html, css } from 'lit';
+import { UserIP } from './UserIP.js';
+
+export class LocationFromIP extends LitElement {
+  static get tag() {
+    return 'location-from-ip';
+  }
+
+  constructor() {
+    super();
+    this.UserIpInstance = new UserIP();
+    this.locationEndpoint = 'https://freegeoip.app/json/';
+    this.long = 10.305385;
+    this.lat = 77.923029;
+  }
+
+  static get properties() {
+    return {};
+  }
+
+  firstUpdated(changedProperties) {
+    if (super.firstUpdated) {
+      super.firstUpdated(changedProperties);
+    }
+    this.getGEOIPData();
+  }
+
+  async getGEOIPData() {
+    const IPClass = new UserIP();
+    const userIPData = await IPClass.updateUserIP();
+    return fetch(this.locationEndpoint + userIPData.ip)
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        return false;
+      })
+      .then(data => {
+        console.log(data);
+        return data;
+      });
+  }
+
+  static get styles() {
+    return [
+      css`
+        :host {
+          display: block;
+        }
+        iframe {
+          height: 500px;
+          width: 500px;
+        }
+      `,
+    ];
+  }
+
+  render() {
+    const url = `https://maps.google.com/maps?q=${this.long},${this.lat}&hl=es;z=14&amp;output=embed`;
+    return html`<iframe title="Where you are" src="${url}"></iframe> `;
+  }
+}
+
+customElements.define(LocationFromIP.tag, LocationFromIP);
